d381883fcc6c6d5f13326681de6b498b11bdff99
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2021-09-27 22:35:13 +0330

    Example-Code

--- Example-Code.php
+++ Example-Code.php
@@ -1,11 +1,11 @@
 <?php
-	error_reporting(0);
-	include 'CryptMe.php';
-	$crypt = new CryptMe();
-	
-	// EnCode:
-	echo $crypt->encode("iNeoTeam");
-	
-	// DeCode:
-	echo $crypt->decode("UUl5K0h3aFBWM1Z4ZzRhVk0xeFJXUT09");
+error_reporting(0);
+include 'CryptMe.php';
+$crypt = new CryptMe();
+
+// EnCode:
+echo $crypt->encode("iNeoTeam");
+
+// DeCode:
+echo $crypt->decode("UUl5K0h3aFBWM1Z4ZzRhVk0xeFJXUT09");
 ?>
